e42e02f0ac2816a1d8799d9bceeb16210ac5ef7e
Lauren Bentley <laurendebentley@gmail.com>
2023-02-25 10:56:26 -0500

    finished put request, all routes functional, i'm literally so proud i'm losing it

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,20 +48,25 @@ router.post('/', async (req, res) => {
 
 
 
-router.put('/:id', async  (req, res) => {
-  // update a category by its `id` value
-  const updatedCategory = await 
-  Category.update(req.body, {
-    where:{
-      id: req.body.id,
+router.put('/:id', (req, res) => {
+  // Calls the update method on the Book model
+  Category.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      category_name: req.body.category_name
+    },
+    {
+      // Gets the books based on the isbn given in the request parameters
+      where: {
+        id: req.params.id,
+      },
     }
-  })
-  if(!updatedCategory){
-    res.status(404).json({message: 'no Categories found, could not update.'})
-  } else {
-    res.status(200).json(updatedCategory)
-  }
-
+  )
+    .then((updatedCategory) => {
+      // Sends the updated book as a json response
+      res.json(updatedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 
